c3f8d80b678891e92f0c43b135191b85f235c66b
mashiike <ikeda-masashi@kayac.com>
2019-06-09 19:52:49 +0900

    fix: use new golicko interface. use json output

--- cmd/rating/main.go
+++ cmd/rating/main.go
@@ -1,33 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"io"
 	"log"
 	"os"
+	"time"
 
 	"github.com/jszwec/csvutil"
-	"github.com/mashiike/golicko/rating"
+	"github.com/mashiike/golicko"
 	"github.com/pkg/errors"
 )
 
 type player struct {
-	ID            string `csv:"player_id"`
-	rating.Rating `csv:"rating"`
+	ID string `csv:"player_id"`
+	*golicko.Player
 }
 
 type match struct {
-	PlayerID         string  `csv:"player_id"`
-	OpponentPlayerID string  `csv:"opponent_player_id"`
-	Score            float64 `csv:"score"`
+	LeftID    string    `csv:"left_id"`
+	RightID   string    `csv:"right_id"`
+	WinnerID  string    `csv:"winner_id"`
+	OutcomeAt time.Time `csv:"outcome_at"`
 }
 
 func main() {
 	var (
-		playersFile = flag.String("p", "./example_players.csv", "players list")
+		playersFile = flag.String("p", "./example_players.json", "players list")
 		matchesFile = flag.String("m", "./example_matches.csv", "matches list")
-		outputFile  = flag.String("o", "./output.csv", "after updated players list")
+		outputFile  = flag.String("o", "./output.json", "after updated players list")
 		tau         = flag.Float64("tau", 0.5, "tau value (0.3 ~ 1.6) default 0.5")
 	)
 	flag.Parse()
@@ -50,38 +55,47 @@ func main() {
 }
 
 func updateRatings(players []player, matches []match, tau float64) []player {
-	ratings := make(map[string]rating.Rating, len(players))
-	opponents := make(map[string][]rating.Rating, len(players))
-	scores := make(map[string][]float64, len(players))
-	initialize := func(l, c int) ([]rating.Rating, []float64) {
-		return make([]rating.Rating, l, c), make([]float64, l, c)
-	}
+	config := golicko.NewConfig()
+	loc, _ := time.LoadLocation("Asia/Tokyo")
+	baseTime := time.Date(2019, 05, 01, 0, 0, 0, 0, loc)
+	config.Tau = tau
+	playersMap := make(map[string]*golicko.Player, len(players))
 	for _, p := range players {
-		ratings[p.ID] = p.Rating
-		opponents[p.ID], scores[p.ID] = initialize(0, 10)
+		playersMap[p.ID] = p.Player
 	}
-
 	for _, m := range matches {
-		if _, ok := ratings[m.PlayerID]; !ok {
-			ratings[m.PlayerID] = rating.Default(0.06)
-			opponents[m.PlayerID], scores[m.PlayerID] = initialize(0, 10)
+		left, ok := playersMap[m.LeftID]
+		if !ok {
+			left = golicko.NewPlayer(baseTime, config)
+			playersMap[m.LeftID] = left
+
 		}
-		if _, ok := ratings[m.OpponentPlayerID]; !ok {
-			ratings[m.OpponentPlayerID] = rating.Default(0.06)
-			opponents[m.OpponentPlayerID], scores[m.OpponentPlayerID] = initialize(0, 10)
+		right, ok := playersMap[m.RightID]
+		if !ok {
+			right = golicko.NewPlayer(baseTime, config)
+			playersMap[m.RightID] = right
+
 		}
-		opponents[m.PlayerID] = append(opponents[m.PlayerID], ratings[m.OpponentPlayerID])
-		scores[m.PlayerID] = append(scores[m.PlayerID], m.Score)
-		opponents[m.OpponentPlayerID] = append(opponents[m.OpponentPlayerID], ratings[m.PlayerID])
-		scores[m.OpponentPlayerID] = append(scores[m.OpponentPlayerID], 1.0-m.Score)
+		winner, ok := playersMap[m.WinnerID]
+		if !ok {
+			winner = nil
+		}
+		match := &golicko.Match{
+			Left:      left,
+			Right:     right,
+			Winner:    winner,
+			OutcomeAt: m.OutcomeAt,
+		}
+		if err := match.Apply(config); err != nil {
+			panic(err)
+		}
 	}
 
-	ret := make([]player, 0, len(ratings))
-	for id, r := range ratings {
-		nr, _ := r.Update(opponents[id], scores[id], tau)
+	ret := make([]player, 0, len(playersMap))
+	for id, p := range playersMap {
 		ret = append(ret, player{
 			ID:     id,
-			Rating: nr,
+			Player: p,
 		})
 	}
 	return ret
@@ -94,20 +108,28 @@ func readPlayers(filename string) ([]player, error) {
 	}
 	defer file.Close()
 
-	dec, err := csvutil.NewDecoder(csv.NewReader(file))
-	if err != nil {
-		return nil, errors.Wrap(err, "decode player csv failed")
+	scanner := bufio.NewScanner(file)
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+	lineCount := 0
+
+	for scanner.Scan() {
+		b := scanner.Bytes()
+
+		buf.Write(b)
+		buf.WriteByte(',')
+
+		lineCount += 1
+	}
+	if lineCount == 0 {
+		buf.WriteByte(',')
 	}
 
-	var players []player
-	var p player
-	err = dec.Decode(&p)
-	for err != io.EOF {
-		if err != nil {
-			return nil, errors.Wrap(err, "player decode line failed")
-		}
-		players = append(players, p)
-		err = dec.Decode(&p)
+	data := buf.Bytes()
+	data[len(data)-1] = ']'
+	players := make([]player, lineCount)
+	if err := json.Unmarshal(data, &players); err != nil {
+		return nil, err
 	}
 	return players, nil
 }
@@ -143,11 +165,9 @@ func writePlayers(filename string, players []player) error {
 		return err
 	}
 	defer file.Close()
-	writer := csv.NewWriter(file)
-	enc := csvutil.NewEncoder(writer)
+	enc := json.NewEncoder(file)
 	for _, p := range players {
 		enc.Encode(p)
 	}
-	writer.Flush()
 	return nil
 }
--- player.go
+++ player.go
@@ -10,9 +10,9 @@ import (
 
 //GameCount is (win/lose/draw) count
 type GameCount struct {
-	Win  uint64
-	Lose uint64
-	Draw uint64
+	Win  uint64 `json:"win"`
+	Lose uint64 `json:"lose"`
+	Draw uint64 `json:"draw"`
 }
 
 func (g GameCount) byteLength() int {
@@ -39,66 +39,66 @@ func (g GameCount) String() string {
 
 //Player is rating holder, implements IPlayer
 type Player struct {
-	estimated *rating.Estimated
-	fixedAt   time.Time
-	count     GameCount
+	Estimated *rating.Estimated `json:"estimated"`
+	FixedAt   time.Time         `json:"fixed_at"`
+	GameCount
 }
 
 //NewPlayer is Player constractor
 func NewPlayer(createdAt time.Time, config *Config) *Player {
 
 	return &Player{
-		estimated: rating.NewEstimated(
+		Estimated: rating.NewEstimated(
 			rating.Default(config.InitialVolatility()),
 			config.Tau,
 		),
-		fixedAt: createdAt.Truncate(config.RatingPeriod),
+		FixedAt: createdAt.Truncate(config.RatingPeriod),
 	}
 }
 
 //Rating is current this player's estimated rating
 func (p *Player) Rating() rating.Rating {
-	return p.estimated.Rating()
+	return p.Estimated.Rating()
 }
 
 //Prepare must do before Update
 func (p *Player) Prepare(now time.Time, config *Config) error {
 	//update system tou: maybe not change often
-	p.estimated.Tau = config.Tau
+	p.Estimated.Tau = config.Tau
 
 	//Reflects the previous non-match period.
-	for now.Sub(p.fixedAt) > config.RatingPeriod {
-		if err := p.estimated.Fix(); err != nil {
+	for now.Sub(p.FixedAt) > config.RatingPeriod {
+		if err := p.Estimated.Fix(); err != nil {
 			return err
 		}
-		p.fixedAt = p.fixedAt.Add(config.RatingPeriod)
+		p.FixedAt = p.FixedAt.Add(config.RatingPeriod)
 	}
 	return nil
 }
 
 //Update do Player's rating update.
 func (p *Player) Update(result *MatchResult, _ *Config) error {
-	if p.fixedAt.After(result.OutcomeAt) {
+	if p.FixedAt.After(result.OutcomeAt) {
 		return errors.New("a match from the pasted")
 	}
-	if err := p.estimated.ApplyMatch(result.Opponent, result.Score); err != nil {
+	if err := p.Estimated.ApplyMatch(result.Opponent, result.Score); err != nil {
 		return errors.Wrap(err, "player update")
 	}
 	switch result.Score {
 	case rating.ScoreDraw:
-		p.count.Draw++
+		p.GameCount.Draw++
 	case rating.ScoreLose:
-		p.count.Lose++
+		p.GameCount.Lose++
 	case rating.ScoreWin:
-		p.count.Win++
+		p.GameCount.Win++
 	}
 	return nil
 }
 
 //String is implements fmt.Stringer
 func (p *Player) String() string {
-	b := make([]byte, 0, len(rating.PlusMinusFormat)+p.count.byteLength())
+	b := make([]byte, 0, len(rating.PlusMinusFormat)+p.GameCount.byteLength())
 	b = p.Rating().AppendFormat(b, rating.PlusMinusFormat)
-	b = p.count.appendByte(b)
+	b = p.GameCount.appendByte(b)
 	return string(b)
 }
